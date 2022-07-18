cb8d5e4e9686859839eb00999b2a1161ed701f0f
Riccardo Alni <riccardo.alni@studio.unibo.it>
2022-07-18 18:34:54 +0200

    first operation working

--- client.py
+++ client.py
@@ -14,10 +14,32 @@ try:
     data, server = sock.recvfrom(4096)
     print(data.decode('utf8'))
     
-    command = input()  
-    sent = sock.sendto(command.encode(), server_address)
+    command = input()
+    cmd = command.split()[0]
+    sent = sock.sendto(cmd.encode(), server_address)
+    
+    if cmd == "1" or cmd == "list":
+        data, server = sock.recvfrom(4096)
+        list = data.decode('utf8')
+        if list == "":
+            print("An error has occured")
+        else:
+            print(list)
+        
+    elif cmd == "get":
+        filename = command.split()[1]
+        print("to do")
+        
+    elif cmd == "put":
+        filename = command.split()[1]
+        print("to do")
+        
+    else:
+        print("You chose an invalid command")
+
+
 except Exception as info:
     print (info)
 finally:
-    print("Closing socket")
+    print("Ending connection")
     sock.close()
--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 import socket as sk
 import time
+import os
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+storage_path = os.path.dirname(__file__) + os.path.sep + "storage" + os.path.sep
+
+def getFiles():
+    files = ""
+    for file in os.scandir(storage_path):
+        files += file.name + "\tsize: "+ str(os.path.getsize(storage_path + file.name)/1024) + " KB\n"
+    return files
 
 server_address = ('localhost', 10000)
-print("Starting connection on ", server_address)
+print("Starting connection on ", server_address, "\n")
 sock.bind(server_address)
 
 welcome_message = '\r\nCONNECTION ESTABLISHED\r\n\r\nWelcome, please select a command\r\n\r\n1. list -> get all the files available in the server\r\n2. get _filename_ -> download _filename_ from the server\r\n3. put _filename_ -> upload a file to the server\r\n'
@@ -20,7 +28,21 @@ while True:
         
         data, address = sock.recvfrom(4096)
         cmd = data.decode("utf8")
-        if cmd == "1" or cmd == "2" or cmd == "3":
-            print("Client ",address," chose command %s\n" % cmd)
+        if cmd == "1" or cmd == "list":
+            print("Client ", address," chose command 1-list\n")
+            ans = getFiles()
+            sent = sock.sendto(ans.encode(), address)
+            
+        elif cmd == "get":
+            print("Client ", address," chose command 2-get\n")
+            print("to do")
+            
+        elif cmd == "put":
+            print("Client ", address," chose command 3-put\n")
+            print("to do")
+            
         else:
-            print("%s is an invalid command\n" % cmd)
+            print("Client ", address," chose an invalid command\n")
+            
+        
+            
