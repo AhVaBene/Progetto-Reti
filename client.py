65ffb55f8872b565b05a31bca00c8f1913ca4deb
Riccardo Alni <riccardo.alni@studio.unibo.it>
2022-07-18 16:44:45 +0200

    creating connection between server and client

--- client.py
+++ client.py
@@ -1,2 +1,20 @@
-Client
+import socket as sk
+import time
 
+sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+
+server_address = ('localhost', 10000)
+data = "Connection"
+
+try:
+    print("Trying to connect to server ",server_address)
+    time.sleep((1))
+    sent = sock.sendto(data.encode(), server_address)
+    
+    data, server = sock.recvfrom(4096)
+    print(data.decode('utf8'))
+except Exception as info:
+    print (info)
+finally:
+    print("Closing socket")
+    sock.close()
--- server.py
+++ server.py
@@ -1,2 +1,19 @@
-Server
+import socket as sk
+import time
 
+sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+
+server_address = ('localhost', 10000)
+print("Starting connection on ", server_address)
+sock.bind(server_address)
+
+while True:
+    print("\nWaiting for a client\n")
+    
+    data, address = sock.recvfrom(4096)
+    
+    if data:
+        data1 = "CONNECTION ESTABLISHED"
+        time.sleep(1)
+        sent = sock.sendto(data1.encode(), address)
+        print("Successfully connected to client ", address)
